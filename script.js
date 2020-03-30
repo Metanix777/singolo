31662221b251728c5c4a5b292bbb29818b111856
CTC <CTC@dc.liftservis.by>
2020-03-30 16:46:42 +0300

    fix: burger menu

--- script.js
+++ script.js
@@ -137,5 +137,35 @@ function validateEmail(email)
 }
 
 
+const burger = document.querySelector(".burger");
+const burgerMenu = document.querySelector(".burger_menu");
+const burger_active = document.querySelector(".burger_active")
+const logo = document.querySelector(".logo_burger");
+
+burger.addEventListener('click', ()=>{
+    burgerMenu.style.display = "block";
+    burger_active.style.display = "block";
+    burger.style.display = "none";
+    logo.style.display = "block";
+});
+
+burger_active.addEventListener('click', ()=>{
+    burgerMenu.style.display = "none";
+    burger_active.style.display = "none";
+    burger.style.display = "block";
+    logo.style.display = "none";
+});
+
+burgerMenu.addEventListener('click', ()=>{
+    burgerMenu.style.display = "none";
+    burger_active.style.display = "none";
+    burger.style.display = "block";
+    logo.style.display = "none";
+});
+
+
+
+
+
 
 
